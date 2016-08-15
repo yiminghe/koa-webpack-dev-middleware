a02052de9bd4ae07036792832eaf7bc59dc59d26
Yevhen Shemet <yevhene@gmail.com>
2016-08-15 17:29:16 +0300

    Original middleware is accessible through original

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function middleware(doIt, req, res) {
 
 module.exports = function (compiler, option) {
   var doIt = expressMiddleware(compiler, option);
-  return function*(next) {
+  var wrapper = function*(next) {
     var ctx = this;
     var req = this.req;
     var runNext = yield middleware(doIt, req, {
@@ -31,4 +31,6 @@ module.exports = function (compiler, option) {
       yield *next;
     }
   };
+  wrapper.original = doIt;
+  return wrapper;
 };
