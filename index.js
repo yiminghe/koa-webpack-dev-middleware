a6774c14304b8e1932aede67989d866565b7027d
yiminghe <yiminghe@gmail.com>
2017-02-04 13:54:42 +0800

    preserve properties of express middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var expressMiddleware = require('webpack-dev-middleware');
+'use strict';
+
+const expressMiddleware = require('webpack-dev-middleware');
 
 function middleware(doIt, req, res) {
-  var originalEnd = res.end;
+  const originalEnd = res.end;
 
   return function (done) {
     res.end = function () {
@@ -15,12 +17,13 @@ function middleware(doIt, req, res) {
 }
 
 module.exports = function (compiler, option) {
-  var doIt = expressMiddleware(compiler, option);
-  var koaMiddleware = function*(next) {
-    var ctx = this;
+  const doIt = expressMiddleware(compiler, option);
+
+  const koaMiddleware = function*(next) {
+    const ctx = this;
     ctx.webpack = doIt;
-    var req = this.req;
-    var runNext = yield middleware(doIt, req, {
+    const req = this.req;
+    const runNext = yield middleware(doIt, req, {
       end: function (content) {
         ctx.body = content;
       },
@@ -33,14 +36,9 @@ module.exports = function (compiler, option) {
     }
   };
 
-  var properties = [
-    'getFilenameFromUrl',
-    'waitUntilValid',
-    'invalidate',
-    'close',
-    'fileSystem'
-  ];
-  properties.map(propertyName => koaMiddleware[propertyName] = doIt[propertyName]);
+  Object.keys(doIt).forEach(p => {
+    koaMiddleware[p] = doIt[p];
+  });
 
   return koaMiddleware;
 };
