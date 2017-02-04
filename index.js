59bd1fd9e2113ad12c106d9164ec74f35b477964
yiminghe <yiminghe@gmail.com>
2017-02-04 14:06:09 +0800

    1.4.x for node 6.x

--- index.js
+++ index.js
@@ -3,38 +3,41 @@
 const expressMiddleware = require('webpack-dev-middleware');
 
 function middleware(doIt, req, res) {
-  const originalEnd = res.end;
+  const { end: originalEnd } = res;
 
-  return function (done) {
-    res.end = function () {
+  return (done) => {
+    res.end = function end() {
       originalEnd.apply(this, arguments);
       done(null, 0);
     };
-    doIt(req, res, function () {
+    doIt(req, res, () => {
       done(null, 1);
     })
-  }
+  };
 }
 
-module.exports = function (compiler, option) {
+module.exports = (compiler, option) => {
   const doIt = expressMiddleware(compiler, option);
 
-  const koaMiddleware = function*(next) {
+  function* koaMiddleware(next) {
     const ctx = this;
+    const { req } = ctx;
+
     ctx.webpack = doIt;
-    const req = this.req;
+
     const runNext = yield middleware(doIt, req, {
-      end: function (content) {
+      end(content) {
         ctx.body = content;
       },
-      setHeader: function () {
+      setHeader() {
         ctx.set.apply(ctx, arguments);
       }
     });
+
     if (runNext) {
       yield *next;
     }
-  };
+  }
 
   Object.keys(doIt).forEach(p => {
     koaMiddleware[p] = doIt[p];
